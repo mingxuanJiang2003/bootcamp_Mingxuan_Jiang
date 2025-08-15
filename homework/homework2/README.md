278aeb9dd5e2dd733615be106d1eddaa80cf4240
mingxuanJiang2003 <mj3683@nyu.edu>
2025-08-14 23:01:54 -0400

    Initial commit - Stage 02 setup

---
+++ homework/homework2/README.md
@@ -0,0 +1,3 @@
+# My Project
+This project is for the FE course Stage 02 setup, demonstrating environment, secrets, and Git workflow.
+
---
+++ homework/homework2/src/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+def load_env():
+    load_dotenv()
+
+def get_key(key_name):
+    return os.getenv(key_name)
+
